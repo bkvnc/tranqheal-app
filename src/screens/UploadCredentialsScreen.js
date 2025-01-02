bcd44fe26adcf4b2ce151f6154a8bc7ee0115a9c
mike28211 <mikkominoza@gmail.com>
2025-01-02 13:54:30 +0800

    recipientId/Type

--- src/screens/UploadCredentialsScreen.js
+++ src/screens/UploadCredentialsScreen.js
@@ -128,7 +128,6 @@ export const UploadCredentialsScreen = ({ navigation, route }) => {
         createdAt: new Date(),
         isRead: false,
         notificationType: 'Application',
-        
       });
       console.log('Professional notification added successfully!');
 
@@ -138,7 +137,10 @@ export const UploadCredentialsScreen = ({ navigation, route }) => {
         createdAt: new Date(),
         isRead: false,
         notificationType: 'Application',
-        destination: `/pending-applications`
+        destination: `/pending-applications`,
+        recipientType: 'Organization',
+        recipientId: organizationId,
+
       });
       console.log('Organization notification added successfully!');
 
